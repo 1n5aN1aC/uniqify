8378919a9f6b39c0942e1857e848f4831a8da8ec
1n5aN1aC <joshuavillwo@gmail.com>
2012-11-02 10:33:02 -0700

    derp

--- src/stdinc.h
+++ src/stdinc.h
@@ -7,3 +7,9 @@
 #pragma once
 
 // TODO: reference additional headers your program requires here
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string.hpp>
--- src/uniqify.cpp
+++ src/uniqify.cpp
@@ -4,9 +4,42 @@
 
 using namespace std;
 
-#define HELP "Correct Program Usage:\n-q quickly append files\n-x extract files\n-t print short File list\n-v print a verbose File list\n-d delete files\n-A quickly append all files in current directory\n"
+#define delimiters " 1234567890!@#$%^&*()`~-_=+[{]}|:;'/?.>,<\\\"\n\t\r"
+#define HELP "Correct Program Usage:\nuniqify [<number of threads >]\nNumber of threads will be asumed to be 10 if not specified\nYou must pipe or redirect the input into uniqify\n"
+
+int workers = 10;
 
 int main(int argc, char** argv)
 {
+	if (argc < 2) {
+		cout << Assuming 10 Working Processess << endl;
+		workers = 10;
+	}
+	else if (argc > 3) {
+		cout << HELP << endl << "I can't handle all this awesomness!\n" << "Too many arguments!" << endl;
+	}
+	else {
+		workers = atoi (argv[1]);
+	}
+	
+	cout << workers;
+	
+	
+	bool quit = false;
+	
+	while (!quit) {
+		if (
+		
+		
+		
+		
+	}	
+	
 	return 0;
+}
+
+readLineFromCIN() {
+	cout << "derp";
+	string bufferIn = readFromLine(file, 4096);
+	boost::split(splitAr,newBufferString,boost::is_any_of(delimiters));
 }
