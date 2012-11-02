e101817b93683b6fa4b7c54e3e37724cc376bad0
1n5aN1aC <joshuavillwo@gmail.com>
2012-11-02 16:32:29 -0700

    I hate compiling objects in C/C++

--- src/stdinc.h
+++ src/stdinc.h
@@ -1,22 +1,25 @@
-// stdafx.h : include file for standard system include files,
+// stdinc.h : include file for standard system include files,
 // or project specific include files that are used frequently, but
 // are changed infrequently
 //
+//Only define these the first time
 #define _BSD_SOURCE
+
+#define HELP "Correct Program Usage:\nuniqify [<number of threads >]\nNumber of threads will be asumed to be 10 if not specified\nYou must pipe or redirect the input into uniqify\n"
+#define PIPE_MAX 4096
+#define STDIN 0
+#define delimiters " 1234567890!@#$%^&*()`~-_=+[{]}|:;'/?.>,<\\\"\n\t\r"
 
 #pragma once
 
-// TODO: reference additional headers your program requires here
+// Reference additional headers your program requires here
+#include <string>
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string.hpp>
-
-#ifndef main
-#define main
-
-#include "Parser.hpp"
-
-#endif
+#include <deque>
+#include <list>
+#include <queue>
--- src/uniqify.cpp
+++ src/uniqify.cpp
@@ -1,21 +1,26 @@
-// myar.cpp : main project file.
+// main.cpp : main project file.
 
 #include "stdinc.h"
 
 using namespace std;
 
-#define HELP "Correct Program Usage:\nuniqify [<number of threads >]\nNumber of threads will be asumed to be 10 if not specified\nYou must pipe or redirect the input into uniqify\n"
-
 int workers = 10;
+
+//Method Declarations
+bool kill(string message);
+bool error(string message);
+string readPipeLine(FILE *file);
 
 int main(int argc, char** argv)
 {
 	if (argc < 2) {
-		cout << Assuming 10 Working Processess << endl;
+		cout << "Assuming 10 Working Processess" << endl;
 		workers = 10;
 	}
-	else if (argc > 3)
+	else if (argc > 3) {
 		cout << HELP << endl << "I can't handle all this awesomness!\n" << "Too many arguments!" << endl;
+		kill("Invalid use of uniq.");
+	}
 	else
 		workers = atoi (argv[1]);
 	
@@ -23,18 +28,60 @@ int main(int argc, char** argv)
 	
 	bool quit = false;
 	while (!quit) {
-		if (
 		
-		
-		
-		
-	}	
-	
+		int filed[2];
+
+		if(pipe(filed) == -1)
+			kill("pipe creation error.");
+
+		switch (fork()) {
+		case -1:
+			kill("error forking!");
+		case 0: //child
+			if (close(filed[1]) == -1)
+				kill("close - child");
+
+			for (;;) {
+				numRead = read(pfd[0], buf, BUF_SIZE);
+				if (numRead == -1)
+					kill("error reading");
+				if (numRead == 0)
+					break;
+				if (write(STDOUT_FILENO, buf numRead) != numRead)
+					kill("partial read/write failure");
+			}
+
+			break;  //leave for safety
+		default: //parent
+			if (close(filed[0]) == -1)
+				kill("close - parent");
+			//write to pipe
+			break;
+		}
+
+		quit = true;
+	}
 	return 0;
 }
 
-readLineFromCIN() {
-	cout << "derp";
-	string bufferIn = readFromLine(file, PIPE_MAX);
-	boost::split(splitAr,newBufferString,boost::is_any_of(delimiters));
+bool kill(string message) { //Ends program with error.
+	perror(message.c_str());
+	exit(-1);
+	return false;
+}
+bool error(string message) {
+	perror(message.c_str());
+	return false;
+}
+
+string readPipeLine(FILE *file) {
+	//assuming FILE is valid if it gets called into here.
+	char *buff = (char*)malloc(PIPE_MAX);
+
+	if(fgets(buff,PIPE_MAX,file) == "eof")
+		return std::string("eof");
+
+	string temp = string(buff);
+	free(buff);
+	return temp;
 }
